6e1566a89ae447b50c86dfae66f3cde52bb79d08
Jonathan Dunlap <jdunlap@outlook.com>
2018-06-12 18:09:02 -0700

    fix compiler

--- src/services/GunService.ts
+++ src/services/GunService.ts
@@ -4,7 +4,7 @@ declare global {
 
 class GunService {
   public connected: boolean = false;
-  private dbSalt: string = "Alice";
+  // private dbSalt: string = "Alice";
   private gun: any = null;
   constructor() {
     this.connected = false;
@@ -13,6 +13,7 @@ class GunService {
     //var chat = gun.get('converse/' + location.hash.slice(1));
   }
   public connect(privateKey: string, dbSalt?: string): void {
+    this.gun.connect();
     this.connected = true;
   }
 }
